e12fbba4c7455d21818e07dfe821f8ee60aa1114
amerlin <yalinwang2@gmail.com>
2025-03-04 20:15:18 +0800

    9090

---
+++ rag/script.py
@@ -0,0 +1,15 @@
+# 选择对应的文本切分器，如果是通用文本的话
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+# 导入文本
+loader = UnstructuredFileLoader("./data/news_test.txt")
+# 将文本转成 Document 对象
+data = loader.load()
+print(f'documents:{len(data)}')
+
+# 初始化加载器
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+# 切割加载的 document
+split_docs = text_splitter.split_documents(data)
+print("split_docs size:",len(split_docs))
